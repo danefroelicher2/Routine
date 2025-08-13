5b9784f999dc27bbd48ba724352892b33f68da8a
danefroelicher2 <froelicherdane@gmail.com>
2025-08-13 10:16:05 -0400

    x

--- src/screens/Auth/LoginScreen.tsx
+++ src/screens/Auth/LoginScreen.tsx
@@ -102,6 +102,7 @@ export default function LoginScreen({ navigation }: LoginScreenProps) {
                                 color="#666"
                             />
                         </TouchableOpacity>
+
                     </View>
 
 
